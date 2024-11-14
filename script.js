d83ed3d3e9d1ab890f6a7519af59ac63f0475cfe
aliya neda <137129762+blackie19@users.noreply.github.com>
2024-11-15 00:03:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 
-const btn = document.querySelector('button');
+/**const btn = document.querySelector('button');
 btn.addEventListener('onclick',function(){
 
    btn.innerText = 'Following';
     btn.style.backgroundColor ="  #45a049;"
-    })
+    })**/
+function changeLabel() {
+            const button = 
+                document.querySelector('button');
+            button.innerHTML = 'following';
+            button.style.backgroundColor ="  #45a049";
+        }
