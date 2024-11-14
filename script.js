b37d9f90941466c789aff63550c145ae29d3ae2e
aliya neda <nedaaliya4@gmail.com>
2024-11-14 22:31:15 +0530

    Second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const btn = document.querySelector('#follow');
+btn. addEventListener('click',function(){
+
+   button.innerHTML = 'Following';
+    button.style.backgroundColor="  #45a049;"
+    })
